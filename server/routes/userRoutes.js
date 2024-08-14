04b619b4707a97bd29cd49b269ea2bd29b496c73
kim-ji-hyun <wlgus95799@naver.com>
2024-08-14 17:44:43 +0900

    코드 수정

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/users", userController.getAllUsers);
 router.get("/users/:id", userController.getOneUser); //동적 라우팅(id가 바뀌는)
-router.post("/user", userController.addOneUser); //동적 라우팅(id가 바뀌는)
+router.post("/user", userController.addUser); //동적 라우팅(id가 바뀌는)
 
 
 export default router;
